a452c389a0ceea00f7cc86c887701597d059e3e0
madoka <madoka.fujimaki@me.com>
2020-03-19 00:48:42 +0100

    added routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Menu from "../views/Menu.vue";
+import OurCoffee from "../views/OurCoffee.vue";
+import OrderStatus from "../views/OrderStatus.vue";
 
 Vue.use(VueRouter);
 
@@ -10,7 +13,21 @@ const routes = [
     name: "Home",
     component: Home
   },
-  {}
+  {
+    path: "/menu",
+    name: "Menu",
+    component: Menu
+  },
+  {
+    path: "/our-coffee",
+    name: "OurCoffee",
+    component: OurCoffee
+  },
+  {
+    path: "/order-status",
+    name: "OrderStatus",
+    component: OrderStatus
+  }
 ];
 
 const router = new VueRouter({
