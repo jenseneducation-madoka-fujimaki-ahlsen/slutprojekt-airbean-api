28ab47fe9614d8c1bae407a1040fd89d44c915ba
jensen-education-razieh-rouzbahani <razie.rouzbahani@gmail.com>
2020-03-19 07:53:12 -0700

    title and text added
    title and text added

--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,32 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+  state: { 
+    title: 'Vårt kaffe',
+    text: 'Pumpkin spice mug, barista cup, sit macchiato, kopi-luwak, doppio, grounds dripper, crema, strong whipped, variety extra iced id lungo half and half mazagran. Pumpkin spice. Que dark fair trade, spoon decaffeinated, barista wings whipped, as rich aftertaste, con panna milk black, arabica white rich beans single shot extra affogato. So affogato macchiato sit extraction instant grinder seasonal organic, turkish single shot, single origin, and robusta strong to go so dripper. Viennese froth, grounds caramelization skinny aromatic cup kopi-luwak, fair trade flavour, frappuccino medium, café au lait flavour cultivar ut bar instant kopi-luwak.Roast id macchiato, single shot siphon mazagran milk fair trade est aroma a half and half and, so, galão iced to go, whipped as cream cup pumpkin spice iced. At extra, rich grinder, brewed to go,steamed half and half at, that, percolator macchiato trifecta and body as arabica dripper. In galão black java milk sit trifecta, robusta, acerbic café au lait instant shop latte. Seasonal bar shop filter aroma id, crema, affogato viennese cultivar aftertaste, seasonal, percolator cream black, galão flavour, milk aromatic turkish skinny crema.',
+    imagePath: './assets/graphics/close.svg',
+
+    products: [], 
   },
-  mutations: {
+  mutations: { 
+    setProducts (state, products) {
+      // update products
+      state.products = products
+    }
   },
-  actions: {
+  actions: { // = methods
+    fetchProducts () {
+      // make the api call to fetch the products
+      // run setProducts mutations
+    }
+  },
+  getters: { 
+    productsCount () {
+      //... return the length of the products array
+    },
+    getImage (state) {
+      return state.imagePath
+    }
   },
   modules: {
   }
