d47498e3cefc144a09fbede12e51919e7c55fb48
jensen-education-razieh-rouzbahani <razie.rouzbahani@gmail.com>
2020-03-20 02:24:52 -0700

    updated
    updated

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -3,23 +3,31 @@
     <div>
         <h1>{{ title }}</h1>
         <p>{{ text }}</p>
+        <img src="@/assets/graphics/eva-cortado.jpg">
+        <h2>{{ name }}</h2>
+        <h3>{{ occupation }}</h3>
+
         
     </div>
-
 </div>
 </template>
 
 <script>
 import store from '@/store/index'
-import { mapState } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 
 export default {
     name: 'Background',
     computed: {
         ...mapState([
             'title',
-            'text'
+            'text',
+            'name',
+            'occupation'
         ]),
+        ...mapGetters({
+            pathImage: 'getImage'
+        }),
         products () {
             return store.state.products
         }
