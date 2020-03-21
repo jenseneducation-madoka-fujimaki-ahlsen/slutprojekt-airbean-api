6419d76acc4a7c3b04496b71c074eade04b74c3c
jensen-education-razieh-rouzbahani <razie.rouzbahani@gmail.com>
2020-03-21 15:18:12 -0700

    OurCoffee updated
    OurCoffee updated

--- src/views/OurCoffee.vue
+++ src/views/OurCoffee.vue
@@ -1,14 +1,19 @@
 <template>
-<div class="ourCoffee">
-    <Background msg="Welcome to Your Vue.js App"/>
+  <div class="ourCoffee">
+    <Nav />
+    <Background />
   </div>
 </template>
 
 <script>
-import Background from '@/components/Background.vue'
+
+import Nav from "@/components/Nav";
+import Background from "@/components/Background.vue";
+
 export default {
-  name: 'Ourcoffee',
+  name: "Ourcoffee",
   components: {
+    Nav,
     Background
   }
 };
