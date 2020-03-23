171569bcade3ff1bc3530521ab6df59925f3f9eb
madoka <madoka.fujimaki@me.com>
2020-03-23 17:37:29 +0100

    changed back store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import * as API from '@/api/index.js';
-import json from "@/assets/data/menu.json";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    menu: json.menu,
-    menuApi: [],
+    menu: [],
     cart: [], 
   },
   mutations: {
