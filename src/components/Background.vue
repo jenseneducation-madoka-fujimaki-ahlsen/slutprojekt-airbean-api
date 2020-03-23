c266731aeff5986310a1c618c743651a7affe6bc
jensen-education-razieh-rouzbahani <razie.rouzbahani@gmail.com>
2020-03-23 03:48:03 -0700

    changed to local data
    changed to local data

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="background">
-    <div class="our-coffee">
+    <div class="our-coffee-wrap">
         <!-- <img :src="headerImagePath" alt="graphics-header"> -->
-        <!-- <img src="@/assets/graphics/graphics-header.svg" alt=""> -->
+        <!-- <img class="header__img" src="@/assets/graphics/graphics-header.svg" alt=""> -->
       <h1>{{ title }}</h1>
-      <span>
+      <div class="text">
       <p id="text1">{{ text1 }}</p>
       <p id="text2">{{ text2 }}</p>
-      </span>
-      <img src="@/assets/graphics/eva-cortado.jpg" />
+      </div>
+      <img class="eva-cortado__img" src="@/assets/graphics/eva-cortado.jpg" />
       <h2>{{ name }}</h2>
       <h3>{{ occupation }}</h3>
     </div>
@@ -17,12 +17,18 @@
 
 <script>
 
-import { mapState } from "vuex";
 
 export default {
   name: "Background",
-  computed: {
-    ...mapState(["title", "text1", "text2", "name", "occupation", "headerImagePath"]),
+  
+  data() {
+    return {
+    title: 'Vårt kaffe',
+    text1: 'Pumpkin spice mug, barista cup, sit macchiato, kopi-luwak, doppio, grounds dripper, crema, strong whipped, variety extra iced id lungo half and half mazagran. Pumpkin spice.',
+    text2: 'Que dark fair trade, spoon decaffeinated, barista wings whipped, as rich aftertaste, con panna milk black, arabica white rich beans single shot extra affogato. So affogato macchiato sit extraction instant grinder seasonal organic, turkish single shot, single origin, and robusta strong to go so dripper. Viennese froth, grounds caramelization skinny aromatic cup kopi-luwak, fair trade flavour, frappuccino medium, café au lait flavour cultivar ut bar instant kopi-luwak.Roast id macchiato, single shot siphon mazagran milk fair trade est aroma a half and half and, so, galão iced to go, whipped as cream cup pumpkin spice iced. At extra, rich grinder, brewed to go,steamed half and half at, that, percolator macchiato trifecta and body as arabica dripper. In galão black java milk sit trifecta, robusta, acerbic café au lait instant shop latte. Seasonal bar shop filter aroma id, crema, affogato viennese cultivar aftertaste, seasonal, percolator cream black, galão flavour, milk aromatic turkish skinny crema.',
+    name: 'Eva Cortado',
+    occupation: 'VD & Grundare',
+    }
   }
 };
 </script>
@@ -32,7 +38,7 @@ export default {
 @import url(https://fonts.googleapis.com/css2?family=Work+Sans:wght@700&display=swap);
 @import url(https://fonts.googleapis.com/css2?family=Work+Sans:wght@500;700&display=swap);
 
-.our-coffee {
+.our-coffee-wrap {
   background: rgb(243, 228, 225);
   height: 100vh;
 }
