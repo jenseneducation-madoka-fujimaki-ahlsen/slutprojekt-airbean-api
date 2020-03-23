8e6765b0f7c53b7766acdb9b17a15723892b49f9
jensen-education-razieh-rouzbahani <razie.rouzbahani@gmail.com>
2020-03-23 12:11:10 -0700

    Cart moved to view
    Cart moved to view

---
+++ src/views/Cart.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="cart">
+    <p>{{total}} kr</p>
+      
+  </div>
+</template>
+
+<script>
+export default {
+  computed: { 
+    shoppingCart() {
+        return this.$store.state.cart
+    },
+    totalAmount () {
+        return this.$store.getters.total
+    }
+}
+};
+</script>
+
+<style></style>
