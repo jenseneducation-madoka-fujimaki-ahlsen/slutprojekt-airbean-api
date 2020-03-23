825a81f0edefcabd92e68f5c83881f93a0617768
madoka <madoka.fujimaki@me.com>
2020-03-23 10:40:39 +0100

    removed unnecessary code on app.js

--- airbean-api/app.js
+++ airbean-api/app.js
@@ -1,23 +1,12 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const data = require("./data/menu.json");
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-const beansRouter = require('./routes/beans')
-const PORT = 5000
+const beansRouter = require("./routes/beans");
+const PORT = 5000;
 
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/beans', beansRouter)
-
-
-app.listen(5000, () => console.log(`Server started on port ${PORT}`))
-
-app.get("/api/beans", (req, res) => {
-  setTimeout(() => {
-    res.json(data);
-  });
-});
+app.use(cors());
+app.use(express.json());
+app.use("/api/beans", beansRouter);
 
 app.listen(5000, () => console.log(`Server started on port ${PORT}`));
