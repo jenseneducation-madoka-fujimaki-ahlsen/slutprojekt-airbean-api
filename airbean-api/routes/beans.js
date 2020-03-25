dbae519acb429e8317fe04c3dd84b0ef7fce2f03
richa310878 <54705026+richa310878@users.noreply.github.com>
2020-03-25 14:06:28 +0100

    Home/orderstatus updated

--- airbean-api/routes/beans.js
+++ airbean-api/routes/beans.js
@@ -1,21 +1,22 @@
-const { Router } = require('express')
-const router = new Router()
-const fs = require('fs');
+const { Router } = require("express");
+const router = new Router();
+const fs = require("fs");
+const { generateOrderNr, generateETA } = require("../utils/utils");
 
-router.get('/', async (req, res) => {
-    const menu = fs.createReadStream('data/menu.json');
-    menu.pipe(res);
+router.get("/", async (req, res) => {
+  const menu = fs.createReadStream("data/menu.json");
+  menu.pipe(res);
 });
 
-router.post('/', async (req, res) => {
-    const order = {
-        eta: 13,
-        orderNr: 'SW921389B',
-    }
+router.post("/", async (req, res) => {
+  const order = {
+    eta: generateETA(),
+    orderNr: generateOrderNr()
+  };
 
-    setTimeout(() => {
-        res.send(order);
-    }, 2000);
+  setTimeout(() => {
+    res.send(order);
+  }, 2000);
 });
 
-module.exports = router
+module.exports = router;
---
+++ airbean-api/utils/utils.js
@@ -0,0 +1,13 @@
+function generateOrderNr() {
+  let letters = ["X", "Y", "Z"];
+  return `AB${Date.now()}${
+    letters[Math.floor(Math.random() * letters.length)]
+  }`;
+}
+
+function generateETA() {
+  let eta = 10;
+  return eta + Math.floor(Math.random() * 20);
+}
+
+module.exports = { generateOrderNr, generateETA };
