f41a64335f5a720d29b9370d52a4ae04a2ad8068
jensen-education-razieh-rouzbahani <razie.rouzbahani@gmail.com>
2020-03-30 09:38:05 -0700

    added rainbow animation on black button
    added rainbow animation on black button

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -51,7 +51,7 @@
         <h3 class="total-price">{{ totalAmount }} kr</h3>
         <p class="total-des">inkl moms + drönarleverans</p>
       </div>
-      <button v-on:click="checkOrder" class="button button-black cartr-button">
+      <button v-on:click="checkOrder" class="button-button-black-cartr-button">
         {{ buttonTxt }}
       </button>
     </div>
@@ -224,5 +224,65 @@ export default {
     text-decoration: none;
     padding: 6px;
   }
+  .button-button-black-cartr-button {
+    width: 70%;
+    margin: 40px 0;
+    height: 56px;
+    font-size: 22px;
+    border: none;
+    outline: none;
+    color: #fff;
+    background: #111;
+    cursor: pointer;
+    position: relative;
+    z-index: 0;
+    border-radius: 50px;
+  }
+  
+.button-button-black-cartr-button:before {
+    content: '';
+    background: linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+    position: absolute;
+    top: -2px;
+    left:-2px;
+    background-size: 400%;
+    z-index: -1;
+    filter: blur(5px);
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    animation: glowing 20s linear infinite;
+    opacity: 0;
+    transition: opacity .3s ease-in-out;
+    border-radius: 10px;
+}
+
+.button-button-black-cartr-button:active {
+    color: #000
+}
+
+.button-button-black-cartr-button:active:after {
+    background: transparent;
+}
+.button-button-black-cartr-button:hover:before {
+    opacity: 1;
+}
+
+.button-button-black-cartr-button:after {
+    z-index: -1;
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #111;
+    left: 0;
+    top: 0;
+    border-radius: 10px;
+}
+
+@keyframes glowing {
+    0% { background-position: 0 0; }
+    50% { background-position: 400% 0; }
+    100% { background-position: 0 0; }
+}
 }
 </style>
