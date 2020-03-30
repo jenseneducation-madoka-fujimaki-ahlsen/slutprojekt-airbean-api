099d2da39438a700bd9eada4f6975c62e98a1829
jensen-education-razieh-rouzbahani <razie.rouzbahani@gmail.com>
2020-03-30 03:25:24 -0700

    removed unnecessary code as commented out
    removed unnecessary code as commented out

--- src/router/index.js
+++ src/router/index.js
@@ -4,51 +4,47 @@ import Home from "../views/Home.vue";
 import Menu from "../views/Menu.vue";
 import OurCoffee from "../views/OurCoffee.vue";
 import Orderstatus from "../views/OrderStatus.vue";
-// import Cart from "../views/Cart.vue";
 import Cart from "../components/Cart.vue";
 import Profile from "../views/Profile.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/menu",
-    name: "Menu",
-    component: Menu,
-    children: [{ path: "/cart", component: Cart, name: "Cart" }]
-  },
-  {
-    path: "/ourcoffee",
-    name: "OurCoffee",
-    component: OurCoffee
-  },
-  {
-    path: "/orderstatus",
-    name: "Orderstatus",
-    component: Orderstatus
-  },
-  // ,
-  // {
-  //   path: "/cart",
-  //   name: "Cart",
-  //   component: Cart
-  // }
-  {
-    path: "/profile",
-    name: "Profile",
-    component: Profile
-  }
+const routes = [{
+        path: "/",
+        name: "Home",
+        component: Home
+    },
+    {
+        path: "/menu",
+        name: "Menu",
+        component: Menu,
+        children: [{
+            path: "/cart",
+            component: Cart,
+            name: "Cart"
+        }]
+    },
+    {
+        path: "/ourcoffee",
+        name: "OurCoffee",
+        component: OurCoffee
+    },
+    {
+        path: "/orderstatus",
+        name: "Orderstatus",
+        component: Orderstatus
+    },
+    {
+        path: "/profile",
+        name: "Profile",
+        component: Profile
+    }
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes
 });
 
 export default router;
